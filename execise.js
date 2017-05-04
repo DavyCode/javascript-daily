8170731a1517fb70fa7b0826ce5553091f180ff6
Davy'd <pdaswift3009@gmail.com>
2017-05-04 19:41:26 +0100

    createphoneNumber function

--- execise.js
+++ execise.js
@@ -892,3 +892,53 @@ function findEvenIndex(arr) {
         }
        return -1;
 } 
+
+//OR
+
+function findEvenIndex(arr)
+{
+  for(var i=1; i<arr.length-1; i++) {
+    if(arr.slice(0, i).reduce((a, b) =>  a+b) === arr.slice(i+1).reduce((a, b) =>  a+b)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+
+
+
+
+// Create Phone Number
+
+
+    // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+    // Example:
+
+    // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+    // The returned format must be correct in order to complete this challenge.
+    // Don't forget the space after the closing parenthese!
+
+
+function createPhoneNumber(numbers){
+	var first = numbers.slice(0, 3).join('');
+	var middle = numbers.slice(3, 6).join('');
+	var last = numbers.slice(6).join('');
+	return "("+first+")"+" "+middle+"-"+last;
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+
+//OR
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  for(var i = 0; i < numbers.length; i++){
+    format = format.replace('x', numbers[i]);
+  }
+ return format;
+}
