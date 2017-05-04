bfef811b39e2142d88ddf9743e675122406ba68f
Davy'd <pdaswift3009@gmail.com>
2017-05-04 19:56:11 +0100

    firstNonRepeatingCharacter function

--- execise.js
+++ execise.js
@@ -941,3 +941,44 @@ function createPhoneNumber(numbers){
   }
  return format;
 }
+
+
+
+
+
+// The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+// If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+// If
+
+//     sz is <= 0 or if str is empty return ""
+//     sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+// Examples:
+// revrot("123456987654", 6) --> "234561876549"
+// revrot("123456987653", 6) --> "234561356789"
+// revrot("66443875", 4) --> "44668753"
+// revrot("66443875", 8) --> "64438756"
+// revrot("664438769", 8) --> "67834466"
+// revrot("123456779", 8) --> "23456771"
+// revrot("", 8) --> ""
+// revrot("123456779", 0) --> "" 
+// revrot("563000655734469485", 4) --> "0365065073456944"
+
+
+
+
+
+..........................................................................
+
+
+
+// Write a function named firstNonRepeatingCharacter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return the empty string ("").
+
