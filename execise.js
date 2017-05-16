4b934ee7718ccd27fc9f810eb5186855c9e9989e
Davy'd <pdaswift3009@gmail.com>
2017-05-16 20:35:20 +0100

    include solution function kata

--- execise.js
+++ execise.js
@@ -1195,3 +1195,22 @@ function check (arr){
 
 
 
+
+
+
+
+// In the following 6 digit number:
+
+// 283910
+
+// 91 is the greatest sequence of 2 digits.
+
+// Complete the solution so that it returns the largest five digit number 
+// found within the number given.. The number will be passed in as a string of only digits. It should 
+// return a five digit integer. The number passed may be as large as 1000 digits. 
+
+
+
+// function solution(digits){
+  
+// }
